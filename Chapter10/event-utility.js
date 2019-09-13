ad9875b12697525ab84a35713ccdab857ab645ed
Jabrel18 <54295665+Jabrel18@users.noreply.github.com>
2019-09-12 19:42:08 -0500

    Add files via upload

---
+++ Chapter10/event-utility.js
@@ -0,0 +1,28 @@
+var evt = {
+	addListener: function(obj, type, fn) {
+		if (obj.addEventListener) {
+	} else {
+		obj.attachEvent("on" + type, fn);
+		}
+	},
+	removeListener: function(obj, type, fn) {
+		if (obj.removeEventListener) {
+			obj.removeEventListener(type, fn);
+		} else {
+			obj.detachEvent("on" + type, fn);
+		}
+	},
+	getTarget: function(e) {
+		if (e.target) {
+			return e.target;
+		}
+		return e.srcElement;
+	},
+	preventDefault: function(e) {
+		if (e.preventDefault) {
+			e.preventDefault();
+		} else {
+			e.returnValue = false;
+		}
+	}
+};
